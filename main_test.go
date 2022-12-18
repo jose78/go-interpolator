373fd65398546a16e4e10e1a8cf3374e29ea4083
jose78 <jose.clavero.anderica@gmail.com>
2022-12-18 06:52:20 +0100

    updated test with the new functionality

--- main_test.go
+++ main_test.go
@@ -24,6 +24,15 @@ func TestDo(t *testing.T) {
 				}},
 			want: content{value: "I'm Jose and I want to RESTORE THE SNYDERVERSE because I would like to see a film related with Batman Who Laughs"},
 		},
+		{name: "Should check the content generate by the judo_interpolator is correct using varsContent",
+		args: args{str: "I'm {{ .name | trim }} and I want to {{ .main_topic | upper  }} because I would like to see a film related with {{ .favorite_superhero | title }}",
+			vars: VarsContent{
+				"name":               "            Jose                 ",
+				"main_topic":         "restore the snyderverse",
+				"favorite_superhero": "batman who laughs",
+			}},
+		want: content{value: "I'm Jose and I want to RESTORE THE SNYDERVERSE because I would like to see a film related with Batman Who Laughs"},
+	},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
