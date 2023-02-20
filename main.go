63df7aaa3b403665a39390e289aaabf998af70f4
jose78 <jose.clavero.anderica@gmail.com>
2023-02-21 00:25:40 +0100

    full refactor

--- main.go
+++ main.go
@@ -24,6 +24,13 @@ package interpolator
 import (
 	"bytes"
 	"fmt"
+
+	//"math/rand"
+	"reflect"
+	"regexp"
+
+	//"strconv"
+	"strings"
 	"text/template"
 
 	"github.com/Masterminds/sprig/v3"
@@ -64,10 +71,25 @@ func (body content) Error() error {
 	return fmt.Errorf(body.value)
 }
 
-// Given a string with the templates, it is interpolated with the value of the vars.
-func Do[T ~map[string]interface{}](str string, vars T) Content {
+func ExtractKeys(str string) []string {
+	var replaceRegexPattern = regexp.MustCompile(`{{|\|(.*?)}}|\.|\}}`)
+	var re = regexp.MustCompile(`{{[ ]*.([a-zA-Z\_\-| ]*) [0-9a-zA-Z \[\],.]*[ ]*}}`)
+	lstKeys := []string{}
+	for _, match := range re.FindAllString(str, -1) {
+		keyStracted := replaceRegexPattern.ReplaceAllString(match, "")
+		lstKeys = append(lstKeys, strings.TrimSpace(keyStracted))
+	}
+	return lstKeys
+}
+
+func evaluateVars(mapsContainer map[string]interface{}) {
+
+}
+
+func fnExecuteInterpolator(str string, vars map[string]interface{}, keysEvaluated map[string]string) (string, error) {
+	var dataError *string = nil
 	funcMap := sprig.FuncMap()
-	tmpl, err := template.New("tmpl").Funcs(funcMap).Parse(str)
+	tmpl, err := template.New("template").Funcs(funcMap).Parse(str)
 	if err != nil {
 		panic(err)
 	}
@@ -76,5 +98,84 @@ func Do[T ~map[string]interface{}](str string, vars T) Content {
 	if err != nil {
 		panic(err)
 	}
-	return content{value: tmplBytes.String()}
+	result := tmplBytes.String()
+	lstKeys := ExtractKeys(result)
+	if len(lstKeys) == 0 {
+		lstKeys = ExtractKeys(str)
+		for _, item := range lstKeys {
+			keysEvaluated[item] = ""
+		}
+	} else {
+		for _, item := range lstKeys {
+			value, ok := keysEvaluated[item]
+			if ok {
+				return *dataError, fmt.Errorf("error, cyclic interpolation detected over the key %s", value)
+			}
+			valueEvaluated, err := fnExecuteInterpolator(vars[item].(string), vars, keysEvaluated)
+			if err != nil {
+				return *dataError,  fmt.Errorf("error, generated to execute a recursive interpolation using the key %s with the content %s: %v", item, vars[item].(string), err)
+			}
+			vars[item] = valueEvaluated
+		}
+		result, err = fnExecuteInterpolator(result, vars, keysEvaluated)
+		if err != nil {
+			return *dataError,  fmt.Errorf("error, generated to resolve the text %s: %v", result, err)
+			
+		}
+	}
+	return result, nil
+}
+
+// Given a string with the templates, it is interpolated with the value of the vars.
+func Do(str string, vars map[string]interface{}) (Content, error) {
+
+	var fnEvaluateVars func(internalVars map[string]interface{}, keysEvaluated map[string]string) error
+	fnEvaluateVars = func(internalVars map[string]interface{}, keysEvaluated map[string]string) error {
+		var re = regexp.MustCompile(`{{[ ]*.[0-9a-zA-Z \[\],.|]+[ ]*}}`)
+
+		var str = "Lo cierto es que en estos momentos estamos en las antípodas {{ .Hola }}  sdasd {{ .Adios | title }} vaya ikaos"
+
+		for i, match := range re.FindAllString(str, -1) {
+			fmt.Println(match, "found at index", i)
+		}
+
+		for key, value := range internalVars {
+			if _, ok := keysEvaluated[key]; ok {
+				return fmt.Errorf("error, ciclic evaluation key:[%s] - value:[%s]", key, value)
+			}
+
+			if reflect.TypeOf(value).Name() == "string" {
+				lstMatches := re.FindAllString(str, -1)
+				if len(lstMatches) == 0 {
+					keysEvaluated[key] = ""
+					internalVars[key] = ""
+					//fnExecuteInterpolator(value.(string), internalVars)
+				}
+			}
+		}
+
+		flagNotContentToBeParsed := true
+		var trace string = ""
+
+		for flagNotContentToBeParsed {
+			flagNotContentToBeParsed = false
+			for key, value := range internalVars {
+				trace = trace + fmt.Sprintf("Key:[%s] - value:[%s]\n", key, value.(string))
+				if reflect.TypeOf(value).Name() == "string" && strings.Contains(value.(string), "{{") {
+					if strings.Contains(value.(string), fmt.Sprintf(".%s", key)) {
+						return fmt.Errorf("error, cyclic interpolation with key '%s': %s", key, trace)
+					}
+					result := ""
+					//fnExecuteInterpolator(value.(string), internalVars)
+					vars[key] = result
+					flagNotContentToBeParsed = true
+				}
+			}
+		}
+		return nil
+	}
+
+	keysEvaluated := make(map[string]string)
+	err := fnEvaluateVars(vars, keysEvaluated)
+	return content{value: fnExecuteInterpolator(str, vars, nil)}, err
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,24 @@
+/*
+The main goal of interpolator is to help you to interpolate your vars inside the string and evaluate functions related with this vars. It is dased on the 'go templates' and using the large list of functions provided by the Sprig Functions Project.
+it is an example ahout how to use it:
+
+	package main
+
+	import "github.com/judoDSL/interpolator"
+
+	func main () {
+		values := make(map[string] interface{})
+		values["name"] = "            Jose                 "
+		values["main_topic"] = "restore the snyderverse"
+		values["favorite_superhero"] = "batman who laughs"
+		interpolator.Do("I'm {{ .name | trim }} and I want to {{ .main_topic | upper  }} because I would like to see a film related with {{ .favorite_superhero | title }}", values).Println()
+	}
+
+It would be the result of the execution:
+
+	[jose78@~/ws/test_interpolator] $  go run main.go
+	I'm Jose and I want to RESTORE THE SNYDERVERSE because I would like to see a film related with Batman Who Laughs
+*/
 package interpolator
 
 import (
@@ -6,6 +27,122 @@ import (
 )
 
 func TestDo(t *testing.T) {
+
+	a := map[string]interface{}{
+		"name":               "            Jose                 ",
+		"main_topic":         "restore the snyderverse",
+		"favorite_superhero": "batman who laughs",
+		"the":                "the",
+	}
+
+	//b := map[string]interface{}{
+	//	"house":     "A {{ .the }} casita ",
+	//	"colour":    "rosa,",
+	//	"the":       "la {{ .cosa_rara}}",
+	//	"animal":    "de {{ .the }} mariposa",
+	//	"cosa_rara": "demo_pato {{ .the }}",
+	//}
+
+	c := map[string]interface{}{
+		"house":     "A {{ .the | title }} casita ",
+		"colour":    "rosa,",
+		"the":       "la {{ .cosa_rara | title  }}",
+		"animal":    "de {{ .the | title }} mariposa",
+		"cosa_rara": "demo_pato",
+	}
+
+	type args struct {
+		str  string
+		vars map[string]interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Content
+		wantErr bool
+	}{
+		{"Should check the content generate by the interpolator is correct",
+			args{"I'm {{ .name | trim }} and I want to {{ .main_topic | upper  }} because I would like to see a film related with {{ .favorite_superhero | title }}", a},
+			content{value: "I'm Jose and I want to RESTORE THE SNYDERVERSE because I would like to see a film related with Batman Who Laughs"},
+			false,
+		}, {"Should interpolate all nested templates",
+			args{" {{ .house }} {{ .colour }} {{ .animal }} {{ .cosa_rara }}", c},
+			content{" A La Demo_pato casita  rosa, de La Demo_pato mariposa demo_pato"},
+			false},
+		//{"should fail",
+		//	args{" {{ .house }} {{ .colour }} {{ .the }} {{ .animal }} {{ .cosa_rara}}", b},
+		//	content{" A la demo_pato la demo_pato {{ .the }} casita  rosa, la demo_pato {{ .the }} de la demo_pato {{ .the }} mariposa demo_pato la demo_pato {{ .the }}"},
+		//	true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Do(tt.args.str, tt.args.vars)
+			if tt.wantErr {
+				if (err != nil) != tt.wantErr {
+					t.Errorf("Do() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
+			} else {
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("Do() = %v, want %v", got, tt.want)
+				}
+			}
+		})
+	}
+}
+
+func TestExtractKeys(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{"Should extrac the keys", args{"Lo más importante en la {{ .vida }} es poder decir lo que {{ .quieres | alto }}"}, []string{"vida", "quieres"}},
+		{"Should extrac the keys", args{"la {{ .cosa_rara | title  }}"}, []string{"cosa_rara"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ExtractKeys(tt.args.str); 
+			
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ExtractKeys() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_evaluateVars(t *testing.T) {
+
+	type args struct {
+		mapsContainer map[string]interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			evaluateVars(tt.args.mapsContainer)
+		})
+	}
+}
+
+func Test_fnExecuteInterpolator(t *testing.T) {
+
+	c := map[string]interface{}{
+		"house":     "A {{ .the | title }} casita ",
+		"colour":    "rosa,",
+		"the":       "la {{ .cosa_rara | title  }}",
+		"animal":    "de {{ .the | title }} mariposa",
+		"cosa_rara": "demo_pato",
+		"mapa": "demo_pato",
+	}
+
 	type args struct {
 		str  string
 		vars map[string]interface{}
@@ -13,31 +150,15 @@ func TestDo(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want Content
+		want string
 	}{
-		{name: "Should check the content generate by the interpolator is correct",
-			args: args{str: "I'm {{ .name | trim }} and I want to {{ .main_topic | upper  }} because I would like to see a film related with {{ .favorite_superhero | title }}",
-				vars: map[string]interface{}{
-					"name":               "            Jose                 ",
-					"main_topic":         "restore the snyderverse",
-					"favorite_superhero": "batman who laughs",
-				}},
-			want: content{value: "I'm Jose and I want to RESTORE THE SNYDERVERSE because I would like to see a film related with Batman Who Laughs"},
-		},
-		{name: "Should check the content generate by the interpolator is correct using varsContent",
-			args: args{str: "I'm {{ .name | trim }} and I want to {{ .main_topic | upper  }} because I would like to see a film related with {{ .favorite_superhero | title }}",
-				vars: VarsContent{
-					"name":               "            Jose                 ",
-					"main_topic":         "restore the snyderverse",
-					"favorite_superhero": "batman who laughs",
-				}},
-			want: content{value: "I'm Jose and I want to RESTORE THE SNYDERVERSE because I would like to see a film related with Batman Who Laughs"},
-		},
+		{"should interpolate this", args{"{{ .house }}",c}, "A La Demo_pato casita "},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Do(tt.args.str, tt.args.vars); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Do() = %v, want %v", got, tt.want)
+			got := fnExecuteInterpolator(tt.args.str, tt.args.vars, map[string]string{}); 
+			if got != tt.want {
+				t.Errorf("fnExecuteInterpolator() = %v, want %v", got, tt.want)
 			}
 		})
 	}
