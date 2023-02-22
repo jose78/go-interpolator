a9e71584978c6b3501b32613965febced3e5ed54
jose78 <jose.clavero.anderica@gmail.com>
2023-02-22 23:43:49 +0100

    implemented multireference

--- main.go
+++ main.go
@@ -19,9 +19,10 @@ it is an example ahout how to use it:
 	[jose78@~/ws/test_interpolator] $  go run main.go
 	I'm Jose and I want to RESTORE THE SNYDERVERSE because I would like to see a film related with Batman Who Laughs
 */
-package interpolator
+package main
 
 import (
+	"github.com/judoDSL/interpolator"
 	"bytes"
 	"fmt"
 	"regexp"
@@ -30,14 +31,18 @@ import (
 
 	"github.com/Masterminds/sprig/v3"
 )
-
-var (
-	index int = 0
-)
+	
+	func main () {
+		values := make(map[string] interface{})
+		values["name"] = "            Jose                 "
+		values["main_topic"] = "restore the snyderverse"
+		values["favorite_superhero"] = "batman who laughs"
+		interpolator.Do("I'm {{ .name | trim }} and I want to {{ .main_topic | upper  }} because I would like to see a film related with {{ .favorite_superhero | title }}", values).Println()
+	}
 
 func extractKeys(str string) []string {
-	var replaceRegexPattern = regexp.MustCompile(`{{|\|(.*?)}}|\.|\}}`)
-	var re = regexp.MustCompile(`{{[ ]*.([a-zA-Z\_\-| ]*) [0-9a-zA-Z \[\],.]*[ ]*}}`)
+	var replaceRegexPattern = regexp.MustCompile(`{{|\|(.*?)}}|\}}`)
+	var re = regexp.MustCompile(`{{[ ]*.([a-zA-Z\_\-|. ]*) [0-9a-zA-Z \[\],.]*[ ]*}}`)
 	lstKeys := []string{}
 	for _, match := range re.FindAllString(str, -1) {
 		keyStracted := replaceRegexPattern.ReplaceAllString(match, "")
@@ -48,7 +53,9 @@ func extractKeys(str string) []string {
 
 func fnInterpolateString(str string, vars map[string]interface{}) string {
 	eval := func(strToInterpolate string) (string, error) {
-		result := fnInterpolateString(strToInterpolate, vars)
+		lstKeys := extractKeys(strToInterpolate)
+		result := appendEval(strToInterpolate, lstKeys)
+		result = fnInterpolateString(result, vars)
 		return result, nil
 	}
 	funcMap := sprig.FuncMap()
@@ -65,51 +72,28 @@ func fnInterpolateString(str string, vars map[string]interface{}) string {
 	return tmplBytes.String()
 }
 
-func executeInterpolator(str string, vars map[string]interface{}, currentKey string, keysEvaluated map[string]string) (string, error) {
-	fmt.Printf("\n\n\n*************************************************************\nindex:%v - currentKey:%s, current str:[%s]\n", index, currentKey, str)
-	index = index + 1
 
-	result := fnInterpolateString(str, vars)
-	lstKeys := extractKeys(result)
-	fmt.Printf("generated result:[%s] and list of keys detected:%v", result, lstKeys)
-	if len(lstKeys) == 0 {
-		vars[currentKey] = result
-		lstKeys = extractKeys(str)
-		if len(lstKeys) > 0 {
-			for _, item := range lstKeys {
-				keysEvaluated[item] = ""
-				fmt.Printf("current key: %s, key evaluated: %s\n", currentKey, item)
-			}
-		} else {
-			fmt.Printf("str %s evaluated with 0 items, current list: %s\n", str, currentKey)
-			result = vars[currentKey].(string)
-		}
-	} else {
-		for _, item := range lstKeys {
-			value, ok := keysEvaluated[item]
-			if ok || item == currentKey {
-				return "", fmt.Errorf("error, cyclic interpolation detected over the key %s", value)
-			}
-			valueEvaluated, err := executeInterpolator(vars[item].(string), vars, item, keysEvaluated)
-			if err != nil {
-				return "", fmt.Errorf("error, generated to execute a recursive interpolation using the key '%s' with the content [%s], err: [%v]", item, vars[item].(string), err)
-			}
-			vars[item] = valueEvaluated
-		}
-		fmt.Printf("Maps used to be iterpoltaed:%v", vars)
-		result = fnInterpolateString(result, vars)
+func appendEval(str string, lstKeys []string) string{
+	freq := make(map[string]int)
+	for _, key := range lstKeys {
+		freq[key] = freq[key] + 1
 	}
-	fmt.Printf("returned result:%s\n", result)
-	return result, nil
+	for key, value  := range freq {
+		pattern := fmt.Sprintf("{{[ ]+%s", key)
+		var re = regexp.MustCompile(pattern)
+		lstMatched := re.FindAllString(str, -1)
+		if len(lstMatched) > 0 {
+			match := lstMatched[0]
+			str = strings.Replace(str, match, fmt.Sprintf("%s | eval", match), value)
+		}else {
+			panic("there are a gost key")
+		}
+	}
+	return str 
 }
 
 // // Given a string with the templates, it is interpolated with the value of the vars.
 func Do(str string, vars map[string]interface{}) (string, error) {
-	result, err := executeInterpolator(str, vars, "", map[string]string{})
-	return result, err
-}
-
-func Do2(str string, vars map[string]interface{}) (string, error) {
 	result := str
 	flagAskToResolveInterpolation := false
 
@@ -117,18 +101,7 @@ func Do2(str string, vars map[string]interface{}) (string, error) {
 		lstKeys := extractKeys(result)
 		flagAskToResolveInterpolation = len(lstKeys) == 0
 		if !flagAskToResolveInterpolation {
-			for _, item := range lstKeys {
-				_, ok := vars[item]
-				if !ok {
-					panic("The variable item not exit")
-				}
-				pattern := fmt.Sprintf("{{[ ]+.%s", item)
-				var re = regexp.MustCompile(pattern)
-				for _, match := range re.FindAllString(result, -1) {
-					result = strings.ReplaceAll(result, match, fmt.Sprintf("{{ .%s | eval", item)) 
-				}
-			}
-			result = fnInterpolateString(result, vars)
+			result = appendEval(result, lstKeys)
 		}
 		result = fnInterpolateString(result, vars)
 	}
--- main_test.go
+++ main_test.go
@@ -1,148 +1,8 @@
 package interpolator
 
 import (
-	"reflect"
 	"testing"
 )
-
-//func TestDo(t *testing.T) {
-//
-//	a := map[string]interface{}{
-//		"name":               "            Jose                 ",
-//		"main_topic":         "restore the snyderverse",
-//		"favorite_superhero": "batman who laughs",
-//		"the":                "the",
-//	}
-//
-//	//b := map[string]interface{}{
-//	//	"house":     "A {{ .the }} casita ",
-//	//	"colour":    "rosa,",
-//	//	"the":       "la {{ .cosa_rara}}",
-//	//	"animal":    "de {{ .the }} mariposa",
-//	//	"cosa_rara": "demo_pato {{ .the }}",
-//	//}
-//
-//	c := map[string]interface{}{
-//		"house":     "A {{ .the | title }} casita ",
-//		"colour":    "rosa,",
-//		"the":       "la {{ .cosa_rara | title  }}",
-//		"animal":    "de {{ .the | title }} mariposa",
-//		"cosa_rara": "demo_pato",
-//	}
-//
-//	type args struct {
-//		str  string
-//		vars map[string]interface{}
-//	}
-//	tests := []struct {
-//		name    string
-//		args    args
-//		want    Content
-//		wantErr bool
-//	}{
-//		{"Should check the content generate by the interpolator is correct",
-//			args{"I'm {{ .name | trim }} and I want to {{ .main_topic | upper  }} because I would like to see a film related with {{ .favorite_superhero | title }}", a},
-//			content{value: "I'm Jose and I want to RESTORE THE SNYDERVERSE because I would like to see a film related with Batman Who Laughs"},
-//			false,
-//		}, {"Should interpolate all nested templates",
-//			args{" {{ .house }} {{ .colour }} {{ .animal }} {{ .cosa_rara }}", c},
-//			content{" A La Demo_pato casita  rosa, de La Demo_pato mariposa demo_pato"},
-//			false},
-//		//{"should fail",
-//		//	args{" {{ .house }} {{ .colour }} {{ .the }} {{ .animal }} {{ .cosa_rara}}", b},
-//		//	content{" A la demo_pato la demo_pato {{ .the }} casita  rosa, la demo_pato {{ .the }} de la demo_pato {{ .the }} mariposa demo_pato la demo_pato {{ .the }}"},
-//		//	true},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			got, err := Do(tt.args.str, tt.args.vars)
-//			if tt.wantErr {
-//				if (err != nil) != tt.wantErr {
-//					t.Errorf("Do() error = %v, wantErr %v", err, tt.wantErr)
-//					return
-//				}
-//			} else {
-//				if !reflect.DeepEqual(got, tt.want) {
-//					t.Errorf("Do() = %v, want %v", got, tt.want)
-//				}
-//			}
-//		})
-//	}
-//}
-
-func TestExtractKeys(t *testing.T) {
-	type args struct {
-		str string
-	}
-	tests := []struct {
-		name string
-		args args
-		want []string
-	}{
-		{"Should extrac the keys", args{"Lo más importante en la {{ .vida }} es poder decir lo que {{ .quieres | alto }}"}, []string{"vida", "quieres"}},
-		{"Should extrac the keys", args{"la {{ .cosa_rara | title  }}"}, []string{"cosa_rara"}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := extractKeys(tt.args.str)
-
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ExtractKeys() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_fnExecuteInterpolator(t *testing.T) {
-
-	varsContent := map[string]interface{}{
-		"mix":       "{{ .house }}  {{ .cosa_rara | upper  }} ",
-		"house":     "A {{ .the }} casita",
-		"the":       "la {{ .cosa_rara | title  }}",
-		"animal":    "de {{ .the | title }} mariposa",
-		"cosa_rara": "demo_pato",
-		"mapa":      "demo_pato",
-		"cyclic":    "This is a {{ .cyclic }}",
-		"colour": map[string]interface{}{
-			"red":    "rojo",
-			"blue":   "azul",
-			"pink":   "rosa",
-			"orange": "{{ mapa }}",
-		},
-	}
-
-	type args struct {
-		str  string
-		vars map[string]interface{}
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    string
-		wantErr bool
-	}{
-		{"should interpolate this", args{"{{ .mix }} ", varsContent}, "A la Demo_pato casita  DEMO_PATO  ", false},
-		{"should interpolate this", args{"{{ .house }}  {{ .cosa_rara | upper  }} ", varsContent}, "A la Demo_pato casita  DEMO_PATO ", false},
-		{"should interpolate this", args{"{{ .house }}", varsContent}, "A la Demo_pato casita", false},
-		{"should generate a error of type cyclic", args{"{{ .cyclic }}", varsContent}, "A La Demo_pato casita ", true},
-		{"should interpolate a coolor", args{"{{ .colour.pink }}", varsContent}, "rosa", false},
-		//{"should interpolate a coolor", args{"{{ .colour.orange }}", varsContent}, "demo_pato", false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := Do(tt.args.str, tt.args.vars)
-			if tt.wantErr {
-				if err == nil {
-					t.Errorf("fnExecuteInterpolator() should return an error and returned: %s", got)
-				}
-			} else {
-				if got != tt.want {
-					t.Errorf("fnExecuteInterpolator() = %v, want %v", got, tt.want)
-				}
-			}
-		})
-	}
-}
 
 func Test_fnInterpolateString(t *testing.T) {
 
@@ -184,7 +44,7 @@ func TestDo2(t *testing.T) {
 		"cosa_rara": "demo_pato",
 		"mapa":      "demo_pato",
 		"redirect_pink": "{{ .colour.pink }}",
-		"redirect_orange": "{{ .colour.orange }}",
+		"redirect_orange": "{{ .colour.orange }} {{ .mix }}",
 		"cyclic":    "This is a {{ .cyclic }}",
 		"colour": map[string]interface{}{
 			"red":    "rojo",
@@ -204,14 +64,17 @@ func TestDo2(t *testing.T) {
 		want    string
 		wantErr bool
 	}{
-		//{"simple interpolation", args{"{{ .cosa_rara | upper }}", varsContent}, "DEMO_PATO", false},
-		//{"medium complex interpolation", args{"{{ .mix }}", varsContent}, "A la Demo_pato casita  DEMO_PATO ", false},
-		{"very complex interpolation", args{"{{ .colour.orange |  upper }}", varsContent}, "demo_pato", false},
-		//{"another very complex interpolation", args{"{{ .redirect_pink | upper }}", varsContent}, "ROSA", false},
+		{"simple interpolation", args{"{{ .cosa_rara | upper }}", varsContent}, "DEMO_PATO", false},
+		{"medium complex interpolation", args{"{{ .mix }}", varsContent}, "A la Demo_pato casita  DEMO_PATO ", false},
+		{"very complex interpolation", args{"{{ .colour.orange |  upper }}", varsContent}, "DEMO_PATO", false},
+		{"another very complex interpolation", args{"{{ .redirect_pink | upper }}", varsContent}, "ROSA", false},
+		{"another very complex interpolation", args{"{{ .redirect_pink | upper }} {{ .mix | title }} {{ .mix }}", varsContent}, "ROSA A La Demo_pato Casita  DEMO_PATO  A la Demo_pato casita  DEMO_PATO ", false},
+		{"another very complex interpolation", args{"{{ .redirect_orange | upper }} {{ .mix | title }} {{ .mix }}", varsContent}, "DEMO_PATO A LA DEMO_PATO CASITA  DEMO_PATO  A La Demo_pato Casita  DEMO_PATO  A la Demo_pato casita  DEMO_PATO ", false},
+	
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := Do2(tt.args.str, tt.args.vars)
+			got, err := Do(tt.args.str, tt.args.vars)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Do2() error = %v, wantErr %v", err, tt.wantErr)
 				return
